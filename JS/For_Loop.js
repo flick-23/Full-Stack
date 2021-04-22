e9226138ea9313aa6a3d5ce1df7e73c0817364a9
Venkatesh D <venkigd8@gmail.com>
2021-04-23 00:45:44 +0530

    Added for in and for of code snippets

--- JS/For_Loop.js
+++ JS/For_Loop.js
@@ -12,3 +12,21 @@ for(let i=0;i<5;i++)
 {
     console.log(i);
 }
+
+//forEach loops example
+const numbers = [12,35,65,,123,56,123,451,142,90]
+
+numbers.forEach(function(x){
+    console.log(x);
+});
+
+
+//for of 
+for(let x of numbers){
+    console.log(x);
+}
+
+//for in 
+for(let x in numbers){
+    console.log(x);
+}
